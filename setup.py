9356c1dfedbade8b519a3ba90ee79050d12d34ab
jaivenkateg <jaivenkateg@kpit.com>
2025-11-29 23:35:46 +0530

    project stucture

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 from setuptools import find_packages,setup
 from typing import List
 
-def get_requirments(file_path:str)-->List[str]:
+def get_requirments(file_path:str)->List[str]:
     '''
     This function will return the list of requirmrnts
     '''
+    hypen='-e .'
     requirments=[]
     with open(file_path,'r') as file:
-        requirments.append(file.readline())
-        [req]
+        requirments=file.readlines()
+        requirments=[req.replace('/n','')for req in requirments]
+        if hypen in requirments:
+            requirments.remove(hypen)
 
 setup(
     name="mymlproject",
     version="0.0.1",
     author='jai venkatesh',
     author_email="jaivenkateshofficial@gmail.com",
-    package_data=find_packages(),
-    install_requirements =get_requirments('requirment.txt')
+    package=find_packages(),
+    install_requires =get_requirments('requirment.txt')
 
 )
